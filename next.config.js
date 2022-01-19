23073c138e55618663ec72231b34a3d422b88ae8
Houdhaifa Lebbad <Houdhaifadeveloper@gmail.com>
2022-01-19 07:19:14 +0100

    Update env variables

--- next.config.js
+++ next.config.js
@@ -3,12 +3,4 @@ module.exports = {
   images: {
     domains: ["books.google.com"],
   },
-  env: {
-    URL: "https://bookshelf.vercel.app",
-    API_KEY: "AIzaSyCQDCoLw1tpTqfcNZaLKaWuhdjTPFA_J8c",
-    SENDGRID_API_KEY:
-      "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",
-    JWT_SECRET: "WANT_TOMEET IN THE BRIDGE OF THE FRIDGE",
-    JWT_EXPIRES_IN: "90d",
-  },
 };
