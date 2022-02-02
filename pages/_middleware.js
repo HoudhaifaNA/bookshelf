aba7498718067f83d960647fecc51cc71584e20a
Houdhaifa Lebbad <Houdhaifadeveloper@gmail.com>
2022-02-02 20:44:49 +0100

    Fix errors 2

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,28 +1,5 @@
 /* eslint-disable @next/next/no-server-import-in-page */
 import { NextResponse } from "next/server";
-const handler = async (req) => {
-  try {
-    let url = req.url.split("app")[1];
-    const res = await fetch(
-      `https://bookshelf.vercel.app/api/user/isLoggedIn?token=${req.cookies.auth_token}`
-    );
-    const data = await res.json();
-    console.log(data);
-    if (
-      data.currentUser &&
-      (url === "/login" || url.startsWith("/confirm") || url === "/")
-    ) {
-      return NextResponse.redirect("/mylibrary");
-    } else if (
-      !data.currentUser &&
-      (url === "/mylibrary" || url === "/logout")
-    ) {
-      return NextResponse.redirect("/login");
-    }
-  } catch (err) {
-    console.log({ ...err });
-    return NextResponse.next();
-  }
-};
+const handler = async (req) => {};
 
 export default handler;
