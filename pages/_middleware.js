c50c55c4327ed1b3724e3826d49bc89fda6f671a
Houdhaifa Lebbad <Houdhaifadeveloper@gmail.com>
2022-02-02 20:49:31 +0100

    Fix errors 3

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,11 +3,12 @@ import { NextResponse } from "next/server";
 const handler = async (req) => {
   try {
     let url = req.url.split("app")[1];
+    console.log(`URL ----------------------- ${url}`);
+
     const res = await fetch(
       `https://bookshelf.vercel.app/api/user/isLoggedIn?token=${req.cookies.auth_token}`
     );
     const data = await res.json();
-    console.log(`URL ----------------------- ${url}`);
     if (
       data.currentUser &&
       (url === "/login" || url.startsWith("/confirm") || url === "/")
