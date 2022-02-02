3a62eb0bf35eabbdfa153a5e0472ae73833867c7
Houdhaifa Lebbad <Houdhaifadeveloper@gmail.com>
2022-02-02 21:11:16 +0100

    Fix errors 5

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,34 +1,29 @@
 /* eslint-disable @next/next/no-server-import-in-page */
 import { NextResponse } from "next/server";
 const handler = async (req) => {
-  try {
-    let url = req.url.split("app")[1];
-    console.log(`URL ----------------------- ${url}`);
+  let url = req.url.split("app")[1];
+  console.log(`URL ----------------------- ${url}`);
 
-    if (req.cookies.auth_token) {
-      const res = await fetch(
-        `https://bookshelf.vercel.app/api/user/isLoggedIn?token=${req.cookies.auth_token}`
-      );
-      let data = await res.json();
-      if (
-        data.currentUser &&
-        (url === "/login" || url.startsWith("/confirm") || url === "/")
-      ) {
-        return NextResponse.redirect("/mylibrary");
-      } else if (
-        !data.currentUser &&
-        (url === "/mylibrary" || url === "/logout")
-      ) {
-        return NextResponse.redirect("/login");
-      }
-    } else {
-      if (!data.currentUser && (url === "/mylibrary" || url === "/logout")) {
-        return NextResponse.redirect("/login");
-      }
+  if (req.cookies.auth_token) {
+    const res = await fetch(
+      `https://bookshelf.vercel.app/api/user/isLoggedIn?token=${req.cookies.auth_token}`
+    );
+    let data = await res.json();
+    if (
+      data.currentUser &&
+      (url === "/login" || url.startsWith("/confirm") || url === "/")
+    ) {
+      return NextResponse.redirect("/mylibrary");
+    } else if (
+      !data.currentUser &&
+      (url === "/mylibrary" || url === "/logout")
+    ) {
+      return NextResponse.redirect("/login");
     }
-  } catch (err) {
-    console.log("ERROR ---", { ...err });
-    return NextResponse.next();
+  } else {
+    if (url === "/mylibrary" || url === "/logout") {
+      return NextResponse.redirect("/login");
+    }
   }
 };
 
